e8171e1aa3c232f6c0d37977864414f449091612
charlessamoy2 <peter.samoy@obf.ateneo.edu>
2021-01-18 01:44:14 +0800

    to test production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,8 +60,17 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  #mailcatcher
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  #mailtrap
+  config.action_mailer.default_url_options = { :host => "localhost:3000", protocol: 'http' }
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
+  config.action_mailer.smtp_settings = {
+    :user_name => 'charlessamoy@gmail.com',
+    :password => 'guvbfxqiddmaxtjo',
+    :address => 'smtp.gmail.com',
+    :domain => 'gmail.com',
+    :port => 465,
+    :authentication => "plain",
+    enable_starttls_auto: "true"
+  }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_17_131828) do
+ActiveRecord::Schema.define(version: 2021_01_17_164700) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2021_01_17_131828) do
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string "token"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
