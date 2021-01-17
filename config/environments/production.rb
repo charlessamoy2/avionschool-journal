26d18681f18dc04e9b8ece665d9cb1598089f4ea
charlessamoy2 <peter.samoy@obf.ateneo.edu>
2021-01-18 02:25:47 +0800

    weeee

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -115,10 +115,10 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
-    :port           => 465,
+    :port           => 587,
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
+    :user_name      => 'apikey',
+    :password       => ENV['SENDGRID_API_KEY'],
     :domain         => 'https://avionschool-journal-samoy.herokuapp.com/',
     :enable_starttls_auto => true
   }
