ed3403f1b7718a7b2864c12a8cc3f8a11f7bc6a3
charlessamoy2 <peter.samoy@obf.ateneo.edu>
2021-01-18 02:43:41 +0800

    weeee

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,4 +1,5 @@
 class UserMailer < Devise::Mailer
+    helper :application
     include Devise::Controllers::UrlHelpers
     default template_path: 'users/mailer'
     
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,7 +112,9 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'https://avionschool-journal-samoy.herokuapp.com/'}
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_charset = "utf-8"  
+  config.action_mailer.perform_deliveries = true  
+  config.action_mailer.raise_delivery_errors = true 
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => 587,
