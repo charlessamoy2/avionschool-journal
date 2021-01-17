f58f5fbecadedbee60e0aed44b711dc38f33d12f
charlessamoy2 <peter.samoy@obf.ateneo.edu>
2021-01-18 02:14:04 +0800

    sendgrid pt. 2 electric bugaloo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,15 +111,15 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'smtp.sendgrid.net'}
+  config.action_mailer.default_url_options = { :host => 'https://avionschool-journal-samoy.herokuapp.com/'}
   config.action_mailer.perform_deliveries = true
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
-    :port           => '587',
+    :port           => 587,
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :domain         => 'https://avionschool-journal-samoy.herokuapp.com/',
     :enable_starttls_auto => true
   }
 end
